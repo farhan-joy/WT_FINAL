1fe92ca1b25dfe7b6cd75124cab219bc757feb9c
farhan <farhansadiq4444gmail.com>
2021-08-01 15:29:58 +0600

    Lab task ajax

---
+++ lab task ajax/my_profile.php
@@ -0,0 +1,15 @@
+<html>
+
+<head></head>
+<?php $name ="Farhan";?>
+
+<body>
+<h1 align="center">My Profile</h1>
+<table align="center">
+<tr>
+
+<td><?php echo $name;?></td>
+</tr>
+</table>
+</body>
+</html>
